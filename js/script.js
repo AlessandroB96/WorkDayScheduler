2f199855e63eb47cd9bea3b34741033c18fdd2a6
Alessandro Belaj <abelaj7196@gmail.com>
2022-01-21 13:00:49 -0500

    added input field and save button click events

---
+++ js/script.js
@@ -0,0 +1,20 @@
+let buttonEl = document.querySelectorAll('#save-btn');
+let textDescription = document.getElementById('#text-box')
+
+//lISTEN FOR SAVE BUTTON 
+
+//addeventlistener only searches for 1 action. We need multiple actions from the same ID, so we loop the event listener for each button 
+
+buttonEl.forEach(function(elem) {
+    elem.addEventListener('click', function() {
+       console.log('this button is clicked'); 
+    });
+});
+
+//LISTEN FOR TEXT BOX INPUT
+
+
+
+
+
+  
