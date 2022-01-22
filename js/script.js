d12d3352c7613851a6a4b4f18bf4fc1aa4546913
Alessandro Belaj <abelaj7196@gmail.com>
2022-01-21 22:22:38 -0500

    set local storage

--- js/script.js
+++ js/script.js
@@ -7,7 +7,52 @@ let textDescription = document.querySelectorAll('#text-box');
 
 let timeDisplay = document.querySelector('#clock-display');
 timeDisplay.textContent = moment().format('dddd MMMM Do YYYY');
-document.appendChild(timeDisplay)
+//document.appendChild(timeDisplay)
+
+ //Change Task  background color based on time
+var checkTime = function () {
+
+    //Get Current time
+    var currentTime = moment().format('H');
+
+    var currentTimeNumber = parseInt(currentTime, 10);
+        console.log("current time is = " , currentTimeNumber);
+
+    //get all elements with class "taskarea"
+    var timeBlockElements = textDescription;
+
+
+    //loop through time blocks
+
+    for (var i = 0 ; i < timeBlockElements.length ; i++) {
+        //Get element's ID as a string
+        var elementID = timeBlockElements[i].name;
+
+        //parse string to numbers
+
+        var stringToNumber = parseInt(elementID, 10);
+       
+
+        let timeValue = stringToNumber;
+        
+        //remove any old classes from element
+        $(timeBlockElements[i].name).removeClass(".present .past .future");
+
+        // apply new class if task is present/past/future
+        if (stringToNumber < currentTimeNumber) {
+            $(timeBlockElements[i]).addClass("past");
+           
+        } else if (stringToNumber > currentTimeNumber) {
+            $(timeBlockElements[i]).addClass("future");
+          
+        } else {
+            $(timeBlockElements[i]).addClass("present");
+            
+       }
+    }
+}
+// checkTime every 5 minutes
+setInterval(checkTime(), (1000 * 60) * 5);
 
 //--------------------------------
 
@@ -18,6 +63,7 @@ document.appendChild(timeDisplay)
 buttonEl.forEach(function(elem) {
     elem.addEventListener('click', function() {
        console.log('this button is clicked'); 
+       
     });
 });
 
@@ -29,7 +75,55 @@ textDescription.forEach(function(elem) {
     });
 })
 
+//---------------------------------
+let obj = [];
 
+$(document).on('click', '#save-btn',function() {
 
+    //let tasks = localStorage.getItem('text-box');
+    
 
+      for (let j = 0; j < textDescription.length; j++) {
+
+        obj.push({
+            'textTimeSlot': textDescription[j].name,
+            'textDescription': textDescription[j].value
+    })
+
+    localStorage.setItem('tasks', JSON.stringify(obj));
+
+    var tasks = JSON.parse(localStorage.getItem('tasks'));
+
+ }
+    
+    
+
+/*
+    $("#hour8 .description").val(localStorage.getItem("hour8"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+
+*/
+
+}
+//localStorage.setItem("text-box", textDescription.values());
+
+//let getStoredValue = localStorage.getItem("text-box");
+//console.log(localStorage);
+    )
+
+//----------------------------------
+//SAVING TASKS TO LOCALSTORAGE 
+
+//setItem() --> localStorage.setItem()
+//getItem() --> localStorage.getItem()
+
+//-----------------------------------
   
